f67ca208514f5ef1572d1572aa07b7cde117349e
Sanjay Mahto <mahto.sanjay41@gmail.com>
2017-06-29 00:21:51 +0530

    updated chat app...

--- BitChat/index.js
+++ BitChat/index.js
@@ -5,21 +5,53 @@ var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 var UserChat = require('./UserChat.js');
 var userChatModel = mongoose.model('UserChat')
+// path is used the get the path of our files on the computer
+var path = require('path');
 
+// set the templating engine 
+app.set('view engine', 'jade');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-//});
+//set the views folder
+app.set('views', path.join(__dirname + '/app/views'));
+
 
 
 //For data base connectivity....
-var dbPath  = "mongodb://localhost/chatDab";
+var dbPath  = "mongodb://localhost/chatDatabase";
 
 // command to connect with database
 db = mongoose.connect(dbPath);
 mongoose.connection.once('open', function() {
   console.log("database connection open success");
 });
+
+//localhost...
+ app.get('/', function (req, res) {
+
+ 	 userChatModel.find().sort({date: -1}).limit(10).exec(function(err, result){
+            if(err){
+               res.render('404', {
+                        message: "Unable to connect to DataBase...",
+                        error: "Error in database..."
+                    });
+            }
+            else{
+            	if(result.length == 0)
+            	{
+            		res.render('index',{
+            			chats:result,
+            			nochat:"There is no chat in Database..."
+            		})
+            	}
+                console.log(result);
+                  res.render('index',{
+                  	chats: result
+              });
+              
+            }
+        });// end find
+       
+    });
  
  //to store the cookies...
 app.use(session({
@@ -33,36 +65,13 @@ app.use(session({
     }
 }));
 
-});
-
- session.userName = null;
 
 io.on('connection', function(socket){
-
 
   socket.on('user',function(data){
     socket.user = data;
     console.log(data+ " is online");
     socket.broadcast.emit('chat message', data+" is online");
-if(session.userName == null || session.userName != data)
-{
-	session.userName = data;
- userChatModel.find().sort({date: -1}).limit(10).exec(function(err, result){
-            if(err){
-               io.emit('chat message', "sorry! Database connectivity Error...");
-            }
-            else{
-                console.log(result.message);
-                for (var i = result.length - 1; i >= 0; i--) {
-                  console.log(result[i].message);
-                  io.emit('chat message', result[i].message+' Submitted on: '+result[i].date.toDateString());
-              }
-            }
-
-        });// end find
-
-}
-    // you can allocate variables in socket.
   });
 
   
@@ -73,8 +82,6 @@ if(session.userName == null || session.userName != data)
             date    : new Date(),
             message : socket.user+" : "+msg
             });// end new chat...
-
-          
              newChat.save(function(err){
                 if(err){
                 console.log("there is error in saving the records...");
@@ -111,6 +118,23 @@ socket.on('message', function(msg){
   }); //end socket disconnected
 
 });
+
+app.use(function (err, req, res) {
+	console.log(err.status);
+    //res.status(err.status || 500);
+    if (err.status == 404) {
+        res.render('404', {
+            message: err.message,
+            error: err
+        });
+    } else {
+        res.render('404', {
+            message: err.message,
+            error: err
+        });
+    }
+});
+
 
 http.listen(3000, function(){
   console.log('listening on port:3000');
